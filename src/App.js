5a83a50bf8f4f3866bc4ccd080f08b23b9a2b34e
Dusana Trifkovic <aleksa2601@gmail.com>
2021-06-14 00:38:05 +0200

    Added the components folder and Todo.js file with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
+    </div>
   );
 }
 
