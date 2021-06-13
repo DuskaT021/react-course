0b7083db2ec1603860e93566245e879f455324f5
Dusana Trifkovic <aleksa2601@gmail.com>
2021-06-14 01:23:26 +0200

    Added styling to Modal.js file

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,10 @@
-function Modal(params) {
-
+function Modal() {
+    return (
+        <div className='modal'>
+            <p>Are you sure?</p>
+            <button className='btn btn--alt'>Cancel</button>
+            <button className='btn'>Confirm</button>
+        </div>
+    );
 }
 export default Modal;
