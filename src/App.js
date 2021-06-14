0fa0657b6e6e6bd915fc6f41fd20d938cfa95170
Dusana Trifkovic <aleksa2601@gmail.com>
2021-06-14 20:16:10 +0200

    Added props to Modal.js and Backdrop.js, it works as expected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Todo from './components/Todo';
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
+
 
 function App() {
   return (
@@ -9,8 +8,7 @@ function App() {
       <Todo text='Learn React' />
       <Todo text='Master React' />
       <Todo text='Explore the full React course' />
-      <Modal />
-      <Backdrop />
+
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+//false cause we don't want the modal to be open in the first place
 function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     function deleteHandler() {
+        setModalIsOpen(true);
+        // we're changing the state here 
+
+    }
+    function closeModalHandler() {
+        setModalIsOpen(false);
 
     }
     return (
@@ -8,6 +22,11 @@ function Todo(props) {
             <div className='actions'>
                 <button className='btn' onClick={deleteHandler}>Delete</button>
             </div>
+            {/* If we left these custom components like this they would always render
+            We want them to be displaye only when setModalIsOpen */}
+            {/* To use the state we check the if the Modal is set to truthy */}
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
         </div>
     );
 }
